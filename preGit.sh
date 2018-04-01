58308fd31984e37e95775e70a1bbcf237704e0b3
Mike Chirico (Chico) <mchirico@t.aipiggybot.io>
2018-04-01 14:23:32 -0400

    update

--- preGit.sh
+++ preGit.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-xcodebuild build -sdk iphonesimulator -workspace septa-ios.xcworkspace -scheme Pods-septa-iosTests CODE_SIGNING_REQUIRED=NO
+xcodebuild build clean -sdk iphonesimulator -workspace septa-ios.xcworkspace -scheme Pods-septa-iosTests CODE_SIGNING_REQUIRED=NO
+xcodebuild build  -sdk iphonesimulator -workspace septa-ios.xcworkspace -scheme Pods-septa-iosTests CODE_SIGNING_REQUIRED=NO
 
 # This works
 xcodebuild test -workspace septa-ios.xcworkspace -scheme Pods-septa-iosTests CODE_SIGNING_REQUIRED=NO -destination 'platform=iOS Simulator,name=iPhone X,OS=11.3'
