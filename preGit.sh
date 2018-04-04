a9c18c33f301b92375bf3bbfb45e5c33d6fa96f3
Mike Chirico (Chico) <mchirico@t.aipiggybot.io>
2018-04-04 15:40:22 -0400

    Travis test

--- preGit.sh
+++ preGit.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 git add Pods
 
-xcodebuild build clean -sdk iphonesimulator -workspace septa-ios.xcworkspace -scheme Pods-septa-iosTests CODE_SIGNING_REQUIRED=NO
-xcodebuild build  -sdk iphonesimulator -workspace septa-ios.xcworkspace -scheme Pods-septa-iosTests CODE_SIGNING_REQUIRED=NO
+xcodebuild clean build -sdk iphonesimulator -workspace septa-ios.xcworkspace -scheme Pods-septa-iosTests CODE_SIGNING_REQUIRED=NO | xcpretty -c
+#xcodebuild build  -sdk iphonesimulator -workspace septa-ios.xcworkspace -scheme Pods-septa-iosTests CODE_SIGNING_REQUIRED=NO -enableCodeCoverage YES
 
 # This works
-xcodebuild test -workspace septa-ios.xcworkspace -scheme Pods-septa-iosTests CODE_SIGNING_REQUIRED=NO -destination 'platform=iOS Simulator,name=iPhone X,OS=11.3'
+xcodebuild test -workspace septa-ios.xcworkspace -scheme Pods-septa-iosTests CODE_SIGNING_REQUIRED=NO -destination 'platform=iOS Simulator,name=iPhone X,OS=11.3' -enableCodeCoverage YES | xcpretty -c
 
 
 xctool -workspace septa-ios.xcworkspace -scheme Pods-septa-iosTests run-tests CODE_SIGNING_REQUIRED=NO -destination 'platform=iOS Simulator,name=iPhone X,OS=11.3' -sdk iphonesimulator
